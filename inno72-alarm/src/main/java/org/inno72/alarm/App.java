23ec13783dedcbf68a8f0a7d5f1a8c6997ccec86
lzh <lizonghao@inno72.com>
2018-09-05 10:40:35 +0800

    init alarm

---
+++ inno72-alarm/src/main/java/org/inno72/alarm/App.java
@@ -0,0 +1,13 @@
+package org.inno72.alarm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
